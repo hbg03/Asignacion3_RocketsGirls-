44295edf15e2d4cdb852197942511f5c586b8aa1
hbg03 <GlorianaHB@estudiantec.cr>
2021-12-12 12:31:21 -0600

    Files
    Volví a crear las clases o los files que Valeria ya había creado

---
+++ gato.js
@@ -0,0 +1,5 @@
+let nombre = "Torvi";
+let peso = 3;
+let color = "Gris";
+let raza = "angora";
+alert("Saluda a la gata" + nombre + "es de color" + color + "y de raza" + raza);
---
+++ java.js
@@ -0,0 +1,1 @@
+alert("Bienvenidas a la veterinaria");
